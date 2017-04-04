c7f13f0c22a8371f39956b6988235e261e169812
Qingsheng <xhy2phd@rice.edu>
2017-04-03 23:48:21 -0500

    Fourth Commit

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -2,8 +2,8 @@
 // Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 #include <RcppEigen.h>
-//[[Rcpp::dependens(RcppEigen)]]
-#include <Rcpp.h>
+//[[Rcpp::depends(RcppEigen)]]
+//#include <Rcpp.h>
 
 using namespace Rcpp;
 using namespace RcppEigen;
